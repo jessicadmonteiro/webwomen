546cc4caa70fc15b3a9e6e8da7d11e9b79e1c0f8
jessica <jessica_02_06@outlook.com>
2022-10-09 22:18:34 -0300

    webwomen

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,164 @@
-/* Desenvolva sua lógica aqui... */
+const containerReceberVagas = document.querySelector(".container_vagas")
+function criarCardDeVaga(vaga){
+    let id         = vaga.id
+    let titulo     = vaga.title
+    let empresa    = vaga.enterprise
+    let cidade     = vaga.location
+    let descricao  = vaga.descrition
+    let modalidade = vaga.modalities
+
+    let tagLi               = document.createElement("li")
+    let tagH2               = document.createElement("h2")
+    let tagDivEmpresaCidade = document.createElement("div")
+    let tagPEmpresa         = document.createElement("p")
+    let tagPCidade          = document.createElement("p")
+    let TagPDescricao       = document.createElement("p")
+    let tagDivBts           = document.createElement("div")
+    let tagBtHome           = document.createElement("button")
+    let tagBtCandidatar     = document.createElement("button")
+
+    tagLi.classList.add("container_descricao_vaga")
+    tagH2.classList.add("titulo_vaga")
+    tagDivEmpresaCidade.classList.add("container_empresa_cidade")
+    tagPEmpresa.classList.add("empresa", "color_grey2")
+    tagPCidade.classList.add("cidade", "color_grey2")
+    TagPDescricao.classList.add("descricao_vaga", "color_grey2")
+    tagDivBts.classList.add("container_bt_homeOff_Candidatar")
+    tagBtHome.classList.add("bt_home", "bt_borda")
+    tagBtCandidatar.classList.add("bt_candidatar", "bt_borda", "bt_cor")
+
+    
+    tagH2.innerText           = titulo
+    tagPEmpresa.innerText     = empresa
+    tagPCidade.innerText      = cidade
+    TagPDescricao.innerText   = descricao
+    tagBtHome.innerText       = modalidade[0]
+    tagBtCandidatar.innerText = "Candidatar"
+    tagBtCandidatar.id        = id
+
+    tagLi.append(tagH2, tagDivEmpresaCidade, TagPDescricao, tagDivBts)
+    tagDivEmpresaCidade.append(tagPEmpresa, tagPCidade)
+    tagDivBts.append(tagBtHome, tagBtCandidatar)
+
+    return tagLi
+}
+
+function adicionarNaLista (lista, referenciaHtml){
+    referenciaHtml.innerText = ""
+
+    lista.forEach(element => {
+        
+        let vagaCriado = criarCardDeVaga (element)
+        referenciaHtml.appendChild(vagaCriado)
+    })
+
+}
+
+adicionarNaLista (jobsData, containerReceberVagas)
+
+let selecionarVaga = []
+
+function criarCardSelecionado (vaga){
+    const listaVagasSeslecionadas = document.querySelector(".conteiner_receber_vagas_selecionadas")
+
+    let id         = vaga.id
+    let titulo     = vaga.title
+    let empresa    = vaga.enterprise
+    let cidade     = vaga.location
+
+    let tagLi               = document.createElement("li")
+    let tagDivTiuloLixeira  = document.createElement("div")
+    let tagH2               = document.createElement("h2")
+    let tagBtLixeira        = document.createElement("button")
+    let tagDivEmpresaCidade = document.createElement("div")
+    let tagPEmpresa         = document.createElement("p")
+    let tagPCidade          = document.createElement("p")
+
+    tagLi.classList.add("container_vaga_selecionada")
+    tagDivTiuloLixeira.classList.add("container_titulo_btLixeira")
+    tagBtLixeira.classList.add("bt_lixeira")
+    tagH2.classList.add("titulo_vaga_selecionada")
+    tagDivEmpresaCidade.classList.add("container_empresa_cidade")
+    tagPEmpresa.classList.add("empresa_selecionada")
+    tagPCidade.classList.add("cidade_selecionada")
+
+    tagH2.innerText       = titulo
+    tagPEmpresa.innerText = empresa
+    tagPCidade.innerText  = cidade
+    tagBtLixeira.id       = id
+
+    tagBtLixeira.addEventListener("click", () =>{
+        
+        const elementoEncontrado = selecionarVaga.findIndex((element) => element)
+    
+        selecionarVaga.splice(elementoEncontrado,1)
+        
+        tagLi.remove()
+        
+    })
+    
+
+    tagLi.append(tagDivTiuloLixeira, tagDivEmpresaCidade)
+    tagDivTiuloLixeira.append(tagH2, tagBtLixeira)
+    tagDivEmpresaCidade.append(tagPEmpresa, tagPCidade)
+
+    listaVagasSeslecionadas.appendChild(tagLi)
+    
+}
+
+function armazenarDados(){
+    const vagaLocalJoson = localStorage.getItem(selecionarVaga)
+    if(vagaLocalJoson){
+        const vagaLocal = JSON.parse(vagaLocalJoson)
+        return selecionarVaga(vagaLocal)
+    }
+}
+armazenarDados()
+
+function selecionarCandidatura (){
+    const botaoCandidatar = document.querySelectorAll(".bt_candidatar")
+    
+    const botao = botaoCandidatar.forEach((botaoAtual) =>{
+
+        botaoAtual.addEventListener("click", (event) =>{
+            if(botaoAtual.innerText === "Candidatar"){
+
+                event.preventDefault()
+                let elemento = event.target
+                let idElemento = elemento.id
+                let id = parseInt(idElemento)
+                let produtoEncontrado = jobsData.find((element)=>{
+                        return element.id == id
+                })
+
+                if(produtoEncontrado){
+
+                criarCardSelecionado(produtoEncontrado)
+                selecionarVaga.push(produtoEncontrado)
+            }
+            }
+
+            const vagasJson = JSON.stringify(selecionarVaga)
+            localStorage.setItem("vagas", vagasJson)
+    
+            let containerselecionarVagaVazio = document.querySelector(".selecionarVazio")
+    
+            if(selecionarVaga !== []){
+                containerselecionarVagaVazio.style.display = "none"
+            }
+
+            if(botaoAtual.innerText === "Candidatar"){
+                botaoAtual.innerText = "Remover Candidatura"
+            }else{
+                botaoAtual.innerText = "Candidatar"
+            }
+            
+
+        })
+    })
+    
+
+}
+
+selecionarCandidatura ()
+
