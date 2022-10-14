9b0eac6be5d0237b10d164a1d6efed0f1030925b
jessica <jessica_02_06@outlook.com>
2022-10-14 08:42:53 -0300

    remoção das vagas selecionadas pelo botão remover candidatura.

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,164 +1,187 @@
 const containerReceberVagas = document.querySelector(".container_vagas")
-function criarCardDeVaga(vaga){
-    let id         = vaga.id
-    let titulo     = vaga.title
-    let empresa    = vaga.enterprise
-    let cidade     = vaga.location
-    let descricao  = vaga.descrition
-    let modalidade = vaga.modalities
 
-    let tagLi               = document.createElement("li")
-    let tagH2               = document.createElement("h2")
-    let tagDivEmpresaCidade = document.createElement("div")
-    let tagPEmpresa         = document.createElement("p")
-    let tagPCidade          = document.createElement("p")
-    let TagPDescricao       = document.createElement("p")
-    let tagDivBts           = document.createElement("div")
-    let tagBtHome           = document.createElement("button")
-    let tagBtCandidatar     = document.createElement("button")
+function criarCardDeVaga(vagas) {
+    containerReceberVagas.innerHTML = ""
 
-    tagLi.classList.add("container_descricao_vaga")
-    tagH2.classList.add("titulo_vaga")
-    tagDivEmpresaCidade.classList.add("container_empresa_cidade")
-    tagPEmpresa.classList.add("empresa", "color_grey2")
-    tagPCidade.classList.add("cidade", "color_grey2")
-    TagPDescricao.classList.add("descricao_vaga", "color_grey2")
-    tagDivBts.classList.add("container_bt_homeOff_Candidatar")
-    tagBtHome.classList.add("bt_home", "bt_borda")
-    tagBtCandidatar.classList.add("bt_candidatar", "bt_borda", "bt_cor")
+    vagas.forEach((element, index) => {
+        let id         = element.id
+        let titulo     = element.title
+        let empresa    = element.enterprise
+        let cidade     = element.location
+        let descricao  = element.descrition
+        let modalidade = element.modalities
 
-    
-    tagH2.innerText           = titulo
-    tagPEmpresa.innerText     = empresa
-    tagPCidade.innerText      = cidade
-    TagPDescricao.innerText   = descricao
-    tagBtHome.innerText       = modalidade[0]
-    tagBtCandidatar.innerText = "Candidatar"
-    tagBtCandidatar.id        = id
+        let tagLi               = document.createElement("li")
+        let tagH2               = document.createElement("h2")
+        let tagDivEmpresaCidade = document.createElement("div")
+        let tagPEmpresa         = document.createElement("p")
+        let tagPCidade          = document.createElement("p")
+        let TagPDescricao       = document.createElement("p")
+        let tagDivBts           = document.createElement("div")
+        let tagBtHome           = document.createElement("button")
+        let tagBtCandidatar     = document.createElement("button")
 
-    tagLi.append(tagH2, tagDivEmpresaCidade, TagPDescricao, tagDivBts)
-    tagDivEmpresaCidade.append(tagPEmpresa, tagPCidade)
-    tagDivBts.append(tagBtHome, tagBtCandidatar)
+        tagLi.classList.add("container_descricao_vaga")
+        tagH2.classList.add("titulo_vaga")
+        tagDivEmpresaCidade.classList.add("container_empresa_cidade")
+        tagPEmpresa.classList.add("empresa", "color_grey2")
+        tagPCidade.classList.add("cidade", "color_grey2")
+        TagPDescricao.classList.add("descricao_vaga", "color_grey2")
+        tagDivBts.classList.add("container_bt_homeOff_Candidatar")
+        tagBtHome.classList.add("bt_home", "bt_borda")
+        tagBtCandidatar.classList.add("bt_candidatar", "bt_borda", "bt_cor")
 
-    return tagLi
-}
 
-function adicionarNaLista (lista, referenciaHtml){
-    referenciaHtml.innerText = ""
+        tagH2.innerText           = titulo
+        tagPEmpresa.innerText     = empresa
+        tagPCidade.innerText      = cidade
+        TagPDescricao.innerText   = descricao
+        tagBtHome.innerText       = modalidade[0]
+        tagBtCandidatar.innerText = "Candidatar"
+        tagBtCandidatar.id        = id
 
-    lista.forEach(element => {
-        
-        let vagaCriado = criarCardDeVaga (element)
-        referenciaHtml.appendChild(vagaCriado)
+        tagLi.append(tagH2, tagDivEmpresaCidade, TagPDescricao, tagDivBts)
+        tagDivEmpresaCidade.append(tagPEmpresa, tagPCidade)
+        tagDivBts.append(tagBtHome, tagBtCandidatar)
+
+        return containerReceberVagas.appendChild(tagLi)
     })
 
 }
+criarCardDeVaga(jobsData)
 
-adicionarNaLista (jobsData, containerReceberVagas)
 
 let selecionarVaga = []
 
-function criarCardSelecionado (vaga){
-    const listaVagasSeslecionadas = document.querySelector(".conteiner_receber_vagas_selecionadas")
+const listaVagasSelecionadas = document.querySelector(".conteiner_receber_vagas_selecionadas")
 
-    let id         = vaga.id
-    let titulo     = vaga.title
-    let empresa    = vaga.enterprise
-    let cidade     = vaga.location
+function criarCardSelecionado(array) {
+    listaVagasSelecionadas.innerHTML = ""
 
-    let tagLi               = document.createElement("li")
-    let tagDivTiuloLixeira  = document.createElement("div")
-    let tagH2               = document.createElement("h2")
-    let tagBtLixeira        = document.createElement("button")
-    let tagDivEmpresaCidade = document.createElement("div")
-    let tagPEmpresa         = document.createElement("p")
-    let tagPCidade          = document.createElement("p")
+    array.forEach((element, index) => {
 
-    tagLi.classList.add("container_vaga_selecionada")
-    tagDivTiuloLixeira.classList.add("container_titulo_btLixeira")
-    tagBtLixeira.classList.add("bt_lixeira")
-    tagH2.classList.add("titulo_vaga_selecionada")
-    tagDivEmpresaCidade.classList.add("container_empresa_cidade")
-    tagPEmpresa.classList.add("empresa_selecionada")
-    tagPCidade.classList.add("cidade_selecionada")
+        let id      = element.id
+        let titulo  = element.title
+        let empresa = element.enterprise
+        let cidade  = element.location
 
-    tagH2.innerText       = titulo
-    tagPEmpresa.innerText = empresa
-    tagPCidade.innerText  = cidade
-    tagBtLixeira.id       = id
+        let tagLi               = document.createElement("li")
+        let tagDivTiuloLixeira  = document.createElement("div")
+        let tagH2               = document.createElement("h2")
+        let tagBtLixeira        = document.createElement("button")
+        let tagDivEmpresaCidade = document.createElement("div")
+        let tagPEmpresa         = document.createElement("p")
+        let tagPCidade          = document.createElement("p")
 
-    tagBtLixeira.addEventListener("click", () =>{
-        
-        const elementoEncontrado = selecionarVaga.findIndex((element) => element)
-    
-        selecionarVaga.splice(elementoEncontrado,1)
-        
-        tagLi.remove()
-        
-    })
-    
+        tagLi.classList.add("container_vaga_selecionada")
+        tagDivTiuloLixeira.classList.add("container_titulo_btLixeira")
+        tagBtLixeira.classList.add("bt_lixeira")
+        tagH2.classList.add("titulo_vaga_selecionada")
+        tagDivEmpresaCidade.classList.add("container_empresa_cidade")
+        tagPEmpresa.classList.add("empresa_selecionada")
+        tagPCidade.classList.add("cidade_selecionada")
 
-    tagLi.append(tagDivTiuloLixeira, tagDivEmpresaCidade)
-    tagDivTiuloLixeira.append(tagH2, tagBtLixeira)
-    tagDivEmpresaCidade.append(tagPEmpresa, tagPCidade)
+        tagH2.innerText       = titulo
+        tagPEmpresa.innerText = empresa
+        tagPCidade.innerText  = cidade
+        tagBtLixeira.id       = index
 
-    listaVagasSeslecionadas.appendChild(tagLi)
-    
-}
+        tagBtLixeira.addEventListener("click", (event) => {
 
-function armazenarDados(){
-    const vagaLocalJoson = localStorage.getItem(selecionarVaga)
-    if(vagaLocalJoson){
-        const vagaLocal = JSON.parse(vagaLocalJoson)
-        return selecionarVaga(vagaLocal)
-    }
-}
-armazenarDados()
+            const id    = event.target.id
+            const botao = event.target
+            const arr   = JSON.parse(localStorage.getItem("vagas")) || []
 
-function selecionarCandidatura (){
-    const botaoCandidatar = document.querySelectorAll(".bt_candidatar")
-    
-    const botao = botaoCandidatar.forEach((botaoAtual) =>{
+            const index = arr.findIndex(element => element.id == id)
 
-        botaoAtual.addEventListener("click", (event) =>{
-            if(botaoAtual.innerText === "Candidatar"){
+            arr.splice(index, 1)
 
-                event.preventDefault()
-                let elemento = event.target
-                let idElemento = elemento.id
-                let id = parseInt(idElemento)
-                let produtoEncontrado = jobsData.find((element)=>{
-                        return element.id == id
-                })
+            localStorage.setItem("vagas", JSON.stringify(arr))
 
-                if(produtoEncontrado){
+            criarCardSelecionado(arr)
 
-                criarCardSelecionado(produtoEncontrado)
-                selecionarVaga.push(produtoEncontrado)
-            }
-            }
-
-            const vagasJson = JSON.stringify(selecionarVaga)
-            localStorage.setItem("vagas", vagasJson)
-    
-            let containerselecionarVagaVazio = document.querySelector(".selecionarVazio")
-    
-            if(selecionarVaga !== []){
-                containerselecionarVagaVazio.style.display = "none"
-            }
-
-            if(botaoAtual.innerText === "Candidatar"){
-                botaoAtual.innerText = "Remover Candidatura"
-            }else{
-                botaoAtual.innerText = "Candidatar"
-            }
             
+            selecionarCandidatura()
+
+            botao.innerText = "Candidatar"
 
         })
+
+
+        tagLi.append(tagDivTiuloLixeira, tagDivEmpresaCidade)
+        tagDivTiuloLixeira.append(tagH2, tagBtLixeira)
+        tagDivEmpresaCidade.append(tagPEmpresa, tagPCidade)
+
+        listaVagasSelecionadas.appendChild(tagLi)
     })
-    
 
 }
+criarCardSelecionado(selecionarVaga)
 
-selecionarCandidatura ()
 
+function selecionarCandidatura() {
+    const botaoCandidatar = document.querySelectorAll(".bt_candidatar")
+
+    const botao = botaoCandidatar.forEach((botaoAtual) => {
+
+        const arr = JSON.parse(localStorage.getItem("vagas")) || []
+
+        const find = arr.find(element => element.id == botaoAtual.id)
+
+        if (find) {
+            botaoAtual.innerText = "Remover Candidatura"
+            botaoAtual.addEventListener("click", remover)
+        } else {
+            botaoAtual.innerText = "Candidatar"
+            botaoAtual.addEventListener("click", adicionar)
+        }
+    })
+}
+selecionarCandidatura()
+
+
+function remover(event) {
+    const id    = event.target.id
+    const botao = event.target
+    const arr   = JSON.parse(localStorage.getItem("vagas")) || []
+
+    const index = arr.findIndex(element => element.id == id)
+
+    arr.splice(index, 1)
+
+    localStorage.setItem("vagas", JSON.stringify(arr))
+
+    criarCardSelecionado(arr)
+
+    botao.innerText = "Candidatar"
+
+    botao.removeEventListener("click", remover)
+    botao.addEventListener("click", adicionar)
+}
+
+
+function adicionar(event) {
+    const id    = event.target.id
+    const botao = event.target
+    const arr   = JSON.parse(localStorage.getItem("vagas")) || []
+
+    const vaga  = jobsData.find(element => element.id == id)
+
+    arr.push(vaga)
+
+    localStorage.setItem("vagas", JSON.stringify(arr))
+
+    criarCardSelecionado(arr)
+
+    let containerselecionarVagaVazio = document.querySelector(".selecionarVazio")
+
+    if (arr !== []) {
+        containerselecionarVagaVazio.style.display = "none"
+    }
+
+    botao.innerText = "Remover Candidatura"
+
+    botao.removeEventListener("click", adicionar)
+
+    botao.addEventListener("click", remover)
+}
